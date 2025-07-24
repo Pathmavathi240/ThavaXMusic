38ea8d85be965054bbe030446b4fcac476025092
Pathmavathi240 <pathmavathi975146@gmail.com>
2025-07-24 13:39:40 +0530

    Update and rename fonts (3).py to fonts.py

--- ThavaXMusic/plugins/tools/fonts.py
+++ ThavaXMusic/plugins/tools/fonts.py
@@ -1,8 +1,8 @@
 from pyrogram import  filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from DeadlineTech.utils.purvi_font import Fonts
-from DeadlineTech import app
+from ThavaXMusic.utils.purvi_font import Fonts
+from ThavaXMusic import app
 
 @app.on_message(filters.command(["font", "fonts"]))
 async def style_buttons(c, m, cb=False):
